10101ab16fb4513a0269fa201c4017dde18a00d2
aymanxoxo <aymanxoxo@hotmail.com>
2018-02-18 14:00:14 +0100

    add analysis class

---
+++ sourceCode/GlobeCurrencyCalculator/AnalysisModule/Analysis.py
@@ -0,0 +1,12 @@
+from GlobeCurrencyCalculator.Enitites import Input, InputType
+
+class Analysis:
+    def DefineSentectType(self, sentence):
+        pass
+    
+    def IsRoman(self, str):
+        pass
+
+    def IsOre(self, str):
+        pass
+    
