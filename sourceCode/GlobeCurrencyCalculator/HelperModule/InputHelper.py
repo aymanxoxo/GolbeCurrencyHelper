4ebe59d5fb26973c9a0e8ee27d3a0c0407037d26
aymanxoxo <aymanxoxo@hotmail.com>
2018-02-18 01:45:52 +0100

    implement input helper

---
+++ sourceCode/GlobeCurrencyCalculator/HelperModule/InputHelper.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+class InputHelper:
+    
+    def ReadLine(self):
+        return stdin.readline()
+
+    def ReadLines(self):
+        return stdin.readlines()
+
+    def ReadFile(self):
+        pass
