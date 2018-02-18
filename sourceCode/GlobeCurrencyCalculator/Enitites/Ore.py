c4b28d13d7fdae2b5f15752a901a51e265f21702
aymanxoxo <aymanxoxo@hotmail.com>
2018-02-18 03:24:43 +0100

    modify ore constructor

--- sourceCode/GlobeCurrencyCalculator/Enitites/Ore.py
+++ sourceCode/GlobeCurrencyCalculator/Enitites/Ore.py
@@ -1,6 +1,6 @@
 from GlobeCurrencyCalculator.Enitites.Input import Input
 
 class Ore(Input):
-    def __init__(self, name, value):
+    def __init__(self, name, value = 0):
         self.Name = name
         self.Value = value
